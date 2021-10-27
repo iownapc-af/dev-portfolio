933f93c60d3ed21ca6db9033764a3ab75c49193c
iownapc-af <cunninghamlogan03@gmail.com>
2021-10-27 16:24:49 -0500

    did some transition/transformy stuff

--- src/Components/Card/Card.tsx
+++ src/Components/Card/Card.tsx
@@ -3,9 +3,8 @@ import './Card.scss';
 export const Card = () => {
   return (
     <div className="content-card" id="content-card">
-      Content
-      <div className="content-card-stuff" id="card-cover">
-        stuff
+      <div className="content-card__content" id="card-cover">
+        Test
       </div>
     </div>
   );
