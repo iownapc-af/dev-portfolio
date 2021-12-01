257c1bf79cd2e5055fcd608dcb4dd2e81cc1c5b5
iownapc-af <cunninghamlogan03@gmail.com>
2021-12-01 12:43:02 -0600

    npc movement is now serverside

--- src/Components/Portfolio/React-Game/Systems/EntityHandler.tsx
+++ src/Components/Portfolio/React-Game/Systems/EntityHandler.tsx
@@ -13,54 +13,6 @@ const EntityHandler = () => {
     });
   });
 
-  useEffect(() => {
-    setInterval(() => {
-      entityMovement();
-    }, 50);
-  }, []);
-
-  const entityMovement = () => {
-    const moveDirection = Math.floor(Math.random() * 6);
-    const entity = Math.floor(Math.random() * npcList.length);
-    if (npcList.length > 0) {
-      switch (moveDirection) {
-        case 0: // up
-          if (npcList[entity].ycoordinate - 18 > 40) {
-            npcList[entity].ycoordinate -= 18;
-          } else {
-            npcList[entity].ycoordinate = 40;
-          }
-          break;
-        case 1: // down
-          if (npcList[entity].ycoordinate - 18 < 554) {
-            npcList[entity].ycoordinate += 18;
-          } else {
-            npcList[entity].ycoordinate = 548;
-          }
-          break;
-        case 2: // left
-          if (npcList[entity].xcoordinate - 18 > 40) {
-            npcList[entity].xcoordinate -= 18;
-          } else {
-            npcList[entity].xcoordinate = 40;
-          }
-          break;
-        case 3: // right
-          if (npcList[entity].xcoordinate - 18 < 754) {
-            npcList[entity].xcoordinate += 18;
-          } else {
-            npcList[entity].xcoordinate = 778;
-          }
-          break;
-        case 4: // Doesn't move if 4/5 are rolled
-        case 5:
-          break;
-      }
-    }
-
-    return null;
-  };
-
   const entityBuilder = () => {
     // if (npcList.length < 1) HELLAFROGS();
     return (
--- src/Components/Portfolio/React-Game/clients/npcClient.ts
+++ src/Components/Portfolio/React-Game/clients/npcClient.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { NPCtype } from '../../../../types/NPCtype';
 
 export const getAllNPCs = (): Promise<NPCtype[]> => {
-  return axios.get('http://localhost:8080/api/npcs').then((res) => {
+  return axios.get('http://localhost:8080/api/npc-moving').then((res) => {
     return res.data.npcs;
   });
 };
